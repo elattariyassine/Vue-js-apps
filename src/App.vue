7acac2207baf899d850dcb13c88a9466c22aa481
El attari Yassine <yassineattari64@gmail.com>
2020-04-05 14:37:19 +0000

    initialize app & creating components

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+    
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function(){
+            return {
+                quotes: ['Just a quote to see something.'],
+                maxQuotes: 10
+            }
+        }
+        
+    }
+</script>
+
+<style>
+</style>
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="row">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        props : ['quotes']
+    }
+</script> 
