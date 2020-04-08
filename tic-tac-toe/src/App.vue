b179596227ca9ffde83f6b704f2d949ea90ab998
El attari Yassine <yassineattari64@gmail.com>
2020-04-08 12:32:45 +0000

    [TIC] creating free project & components & grid.vue styling

---
+++ tic-tac-toe/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+      <h1>Tic Tac Toe Game</h1>
+      <Grid></Grid>
+  </div>
+</template>
+
+<script>
+
+import Grid from './components/Grid.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Grid
+  }
+}
+</script>
+
+<style>
+
+#app {
+  font-family: 'Dosis', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 0 auto;
+  max-width: 400px;
+}
+#app h1{
+  text-transform: uppercase;
+  font-weight: bold;
+  font-size: 3em;
+}
+
+</style>
---
+++ tic-tac-toe/src/components/Grid.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <div class="grid">
+            <div class="cell"></div>
+            <div class="cell"></div>
+            <div class="cell"></div>
+            <div class="cell"></div>
+            <div class="cell"></div>
+            <div class="cell"></div>
+            <div class="cell"></div>
+            <div class="cell"></div>
+            <div class="cell"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="css" scoped>
+.grid {
+    display: grid;
+    grid-template-columns: 120px 120px 120px;
+    grid-gap: 10px;
+    background-color: #34495e;
+    width: 100%;
+    position: relative;
+    padding: 10px;
+    box-sizing: border-box;
+}
+
+.cell {
+    height: 120px;
+    line-height: 120px;
+    font-size: 3.5em;
+    background-color: #2c3e50;
+    font-family: 'Permanent Marker', cursive;
+}
+
+.cell:hover {
+    background-color: #405971;
+    cursor: pointer;
+}
+</style>
